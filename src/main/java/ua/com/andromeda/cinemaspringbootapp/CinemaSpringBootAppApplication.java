eea9acf95bf3ea4ac6e533e9904b76ed8bde85b1
Andrrr <geraskin19970309@gmail.com>
2022-09-12 09:48:05 +0300

    git init

---
+++ src/main/java/ua/com/andromeda/cinemaspringbootapp/CinemaSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package ua.com.andromeda.cinemaspringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaSpringBootAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaSpringBootAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/com/andromeda/cinemaspringbootapp/CinemaSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.andromeda.cinemaspringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaSpringBootAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
