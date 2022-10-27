9c95b09ed643139720c8d687bb8d876ecc975b1f
Andrrr <geraskin19970309@gmail.com>
2022-10-27 12:07:55 +0300

    update for deploy

--- src/main/java/ua/com/andromeda/cinemaspringbootapp/utils/PdfService.java
+++ src/main/java/ua/com/andromeda/cinemaspringbootapp/utils/PdfService.java
@@ -23,7 +23,7 @@ public class PdfService {
         PdfWriter.getInstance(document, new FileOutputStream("tickets.pdf"));
         document.open();
 //        Path path = Paths.get(getClass().getClassLoader().getResource("static/logo.png").toURI());
-        Path path = Paths.get(new ClassPathResource("static/logo.png").getURI());
+        Path path = Paths.get(new ClassPathResource("/static/logo.png").getURI());
         Image image = Image.getInstance(path.toAbsolutePath().toString());
         image.setAlignment(Element.ALIGN_CENTER);
         document.add(image);
